126fd40bcf9c79233c150521a44e314d2fa2af1f
Lenno Sousa <lenno_sousa@hotmail.com>
2022-10-23 11:40:18 -0300

    Class13 - Implementando a restrição da arquitetura de uma API Rest, tornando-a assim, uma API RestFul.

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,16 +1,18 @@
-﻿using RestWithASPNETUdemy.Data.VO;
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace RestWithASPNETUdemy.Data.VO
 {
     [Table("person")]
-    public class PersonVO
+    public class PersonVO : ISupportsHypermedia
     {
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Gender { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
     }
 }
